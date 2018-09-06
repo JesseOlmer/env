77ce98e829951938cf1edb8ec8bedc51a75e8599
Jesse Olmer <jesse@wickedgoodtimes.com>
2018-09-06 00:02:07 -0700

    Fix gnome extension install and separate elevated steps into sub-script

---
+++ elevatedsteps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function requireroot {
+	if ! [ $(id -u) = 0 ]; then
+	   echo "The script needs to be run by escalating to root (sudo)." >&2
+	   exit 1
+	fi
+}
+
+function installzsh {
+	echo "Installing zsh"
+	apt-get update > /dev/null
+	apt-get install -y zsh powerline fonts-powerline > /dev/null
+}
+
+function installhtop {
+	echo "Installing htop"
+	apt-get install -y htop > /dev/null
+}
+
+requireroot
+installzsh
+installhtop
--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-function requireroot {
-	if ! [ $(id -u) = 0 ]; then
-	   echo "The script needs to be run by escalating to root (sudo)." >&2
-	   exit 1
-	fi
-}
-
 function requiresudo {
 	if [ $(id -u $real_user) = 0 ]; then
 		echo "The script should be run by escalating (sudo) from a non-root account." >&2
@@ -14,23 +7,12 @@ function requiresudo {
 	fi
 }
 
-function installzsh {
-	echo "Installing zsh"
-	apt-get update > /dev/null
-	apt-get install -y zsh powerline fonts-powerline > /dev/null
-}
-
-function installhtop {
-	echo "Installing htop"
-	apt-get install -y htop > /dev/null
-}
-
 function installohmyzsh {
-	echo "Installing oh-my-zsh"
 	if [ -d ~/.oh-my-zsh ]; then
 		echo "oh-my-zsh already installed. Skipping"
 		return 0
 	fi
+	echo "Installing oh-my-zsh"
 	sudo -u $real_user sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
 
@@ -40,42 +22,42 @@ function installzshrc {
 }
 
 function installclipboardmgr {
-	echo "Installing clipboard manager"
 	if [ -d ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com ]; then
 		echo "clipboard manager already installed. Skipping"
 		return 0
 	fi
+	echo "Installing clipboard manager"
 
-	sudo -u $real_user git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
-	pushd ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
-	sudo -u $real_user gnome-shell-extension-tool -e clipboard-indicator@tudmotu.com
-	sudo -u $real_user glib-compile-schemas schemas
-	sudo -u $real_user gsettings --schemadir schemas set org.gnome.shell.extensions.shelltile gap-between-windows 3
-	popd
+	git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com > /dev/null
+	pushd ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com > /dev/null
+	gnome-shell-extension-tool -e clipboard-indicator@tudmotu.com
+	glib-compile-schemas schemas
+	gsettings --schemadir schemas set org.gnome.shell.extensions.clipboard-indicator toggle-menu "['<Ctrl>grave']"
+	gsettings --schemadir schemas set org.gnome.shell.extensions.clipboard-indicator notify-on-copy false
+	popd > /dev/null
 }
 
 function installshelltile {
-	echo "Installing shelltile"
 	if [ -d ~/.local/share/gnome-shell/extensions/ShellTile@emasab.it ]; then
 		echo "shelltile already installed. Skipping"
 		return 0
 	fi
-	sudo -u $real_user git clone https://github.com/emasab/shelltile.git ~/.local/share/gnome-shell/extensions/ShellTile@emasab.it
-	pushd ~/.local/share/gnome-shell/extensions/ShellTile@emasab.it
-	sudo -u $real_user gnome-shell-extension-tool -e ShellTile@emasab.it
-	sudo -u $real_user glib-compile-schemas schemas
-	sudo -u $real_user gsettings --schemadir schemas set org.gnome.shell.extensions.clipboard-indicator toggle-menu "['<Ctrl>grave']"
-	sudo -u $real_user gsettings --schemadir schemas set org.gnome.shell.extensions.clipboard-indicator notify-on-copy false
-	popd
+	echo "Installing shelltile"
+	git clone https://github.com/emasab/shelltile.git ~/.local/share/gnome-shell/extensions/ShellTile@emasab.it > /dev/null
+	pushd ~/.local/share/gnome-shell/extensions/ShellTile@emasab.it > /dev/null
+	gnome-shell-extension-tool -e ShellTile@emasab.it
+	glib-compile-schemas schemas
+	gsettings --schemadir schemas set org.gnome.shell.extensions.shelltile gap-between-windows 3
+	popd > /dev/null
 }
 
 function addzshtobashrc {
-	echo "Adding zsh execution to bashrc"
 	grep -Pzoq 'if \[ -t 1 ]; then\n  exec zsh\nfi' ~/.bashrc
 	if [ $? -eq 0 ]; then
 		echo "zsh already in bashrc. Skipping"
 		return 0
 	fi
+	echo "Adding zsh execution to bashrc"
 	sudo -u $real_user cat <<EOT >> ~/.bashrc
 if [ -t 1 ]; then
   exec zsh
@@ -89,10 +71,7 @@ else
     real_user=$(whoami)
 fi
 
-requireroot
-requiresudo
-installzsh
-installhtop
+sudo ./elevatedsteps.sh
 installohmyzsh
 installclipboardmgr
 installshelltile
